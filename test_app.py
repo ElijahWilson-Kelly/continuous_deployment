b72a3ef8b06c2ace7ec54fd351821c9773c56f48
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-07-23 19:12:33 +0200

    added reqiurements

--- test_app.py
+++ test_app.py
@@ -7,11 +7,8 @@ def client():
     app.config.update({
         "TESTING": True,
     })
-    print("Testing")
     client = app.test_client()
-    yield client
-
-    print("All done")
+    return client
 
 def test_get_home_returns_200(client):
     response = client.get("/")
