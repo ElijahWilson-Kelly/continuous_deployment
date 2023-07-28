a4319ee73a67c94acd3ebc0789cafd0e3160a27f
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-07-28 13:12:54 +0200

    added contacts

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
     return "<h1>My name is Elijah</h1>"
+
+@app.route("/contact")
+def contact_page():
+    "<h1>Welcome to the contacts page</h1>"
