00fa267f96a9d0e61f8283ca4b587131910253a6
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-07-28 13:15:11 +0200

    Test change

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "<h1>My name is Elijah</h1>"
+    return "<h1>My name is Fred</h1>"
 
 @app.route("/contact")
 def contact_page():
-    "<h1>Welcome to the contacts page</h1>"
+    return "<h1>Welcome to the contacts page</h1>"
