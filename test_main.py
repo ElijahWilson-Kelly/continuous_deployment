57183073896345e62c35102dbe8e65757bb3163e
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-08-07 09:45:49 +0200

    Added tests

--- test_main.py
+++ test_main.py
@@ -10,7 +10,19 @@ def client():
     client = app.test_client()
     return client
 
-def test_get_home_returns_200(client):
+def test_get_home_returns_302(client):
     response = client.get("/")
+    assert response.status_code == 302
+
+def test_get_login_returns_200(client):
+    response = client.get("/login")
     assert response.status_code == 200
+
+def test_get_dashboard_returns_200(client):
+    response = client.get("/dashboard")
+    assert response.status_code == 200
+
+def test_get_nonexistant_route_returns_404(client):
+    response = client.get("/not-a-route")
+    assert response.status_code == 404
 
